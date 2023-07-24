824c9a5968bd19893cbcb0e612ebf0228936289d
Tantri <tanyaluk.tri@gmail.com>
2023-07-24 16:39:05 +0200

    create get treatment of sever.js

--- phrase1/src/testData/sever.js
+++ phrase1/src/testData/sever.js
@@ -6,13 +6,17 @@ import { JSONFile } from 'lowdb/node';
 
 
 // database setup
-const file = "../data/PatientData.json"
-const adapter = new JSONFile(file)
-const db = new Low(adapter, {})
+const patientsFile = "../data/PatientData.json"
+const treatmentsFile = "../data/treatment.json"
+const patientsAdapter = new JSONFile(patientsFile)
+const treatmentsAdapter = new JSONFile(treatmentsFile)
+const patientsDb = new Low(patientsAdapter, {})
+const treatmentsDb = new Low(treatmentsAdapter, {})
 
 // Read data from JSON file, this will set db.data content
 // If JSON file doesn't exist, defaultData is used instead
-await db.read()
+await patientsDb.read()
+await treatmentsDb.read()
 
 const app = express();
 
@@ -22,7 +26,7 @@ app.use(express.urlencoded());
 
 app.get('/patient/:patientCode', (req, res) => {
     const patientCode = req.params.patientCode;
-    const patients = db.data;
+    const patients = patientsDb.data;
     const patient = patients.find((patient) => {return patient.patientCode === patientCode});
     if (patient) {
         res.set("Access-Control-Allow-Origin", "*")
@@ -31,17 +35,29 @@ app.get('/patient/:patientCode', (req, res) => {
         res.set("Access-Control-Allow-Origin", "*")
         res.sendStatus(404)
     }
-
-
 })
+
+app.get('/treatment/:id', (req, res) => {
+    const treatmentId = req.params.id;
+    const treatments = treatmentsDb.data;
+    const treatment = treatments.find((treatment) => {return treatment.id === treatmentId});
+    if (treatment) {
+        res.set("Access-Control-Allow-Origin", "*")
+        res.json(treatment);
+    } else {
+        res.set("Access-Control-Allow-Origin", "*")
+        res.sendStatus(404)
+    }
+})
+
 
 app.post('/patient/:patientCode', (req, res) => {
     const patientCode = req.params.patientCode;
-    const patients = db.data;
+    const patients = patientsDb.data;
     const patientIndex = patients.findIndex((patient)=> {return patient.patientCode === patientCode})
     if (patientIndex >= 0) {
         patients[patientIndex] = req.body;
-        db.write();
+        patientsDb.write();
         res.sendStatus(200)
     } else {
         res.sendStatus(500)
